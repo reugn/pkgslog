e8a2f4584fe3ecceded7416b28ade217d6df87f2
Eugene R <reugpro@gmail.com>
2024-03-08 10:32:47 +0200

    fix: handling lower log levels for packages (#4)
    * allow pkgslog to accept lower levels than its wrapped handler (#3)
    
    * fix: handling lower log levels for packages
    
    ---------
    
    Co-authored-by: Christian Winther <jippignu@gmail.com>

--- pkgslog_test.go
+++ pkgslog_test.go
@@ -3,12 +3,12 @@ package pkgslog_test
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"encoding/json"
 	"io"
-	"testing"
-
 	"log/slog"
-
+	"slices"
+	"testing"
 	"testing/slogtest"
 
 	"github.com/reugn/pkgslog"
@@ -23,6 +23,8 @@ func BenchmarkPkgSlog(b *testing.B) {
 		"github.com/reugn/pkgslog/pkg/inner": slog.LevelInfo,
 	}
 	logger := slog.New(pkgslog.NewPackageHandler(textHandler, packageMap))
+	b.ResetTimer()
+
 	for i := 0; i < b.N; i++ {
 		logger.Info("here")
 	}
@@ -30,6 +32,8 @@ func BenchmarkPkgSlog(b *testing.B) {
 
 func BenchmarkSlog(b *testing.B) {
 	logger := slog.New(slog.NewTextHandler(io.Discard, nil))
+	b.ResetTimer()
+
 	for i := 0; i < b.N; i++ {
 		logger.Info("here")
 	}
@@ -52,9 +56,104 @@ func TestPkgSlog(t *testing.T) {
 	}
 }
 
+func TestPkgSlogOverrideUpstreamHandlerLogLevel(t *testing.T) { //nolint:funlen
+	levels := []slog.Level{
+		slog.LevelDebug,
+		slog.LevelError,
+		slog.LevelInfo,
+		slog.LevelWarn,
+	}
+
+	tests := []struct {
+		name         string
+		pkgLevel     slog.Level
+		handlerLevel slog.Level
+		enabled      []slog.Level
+		disabled     []slog.Level
+	}{
+		// If
+		// 		"pkgslog" level is set to DEBUG
+		// 		"handler" level is set to WARNING
+		// Then
+		// 		"pkgslog" should accept log levels appropriate
+		// 		to its *minimum* configured log level
+		// Meaning
+		// 		All log levels should be accepted
+		{
+			name:         "debug level",
+			pkgLevel:     slog.LevelDebug,
+			handlerLevel: slog.LevelWarn,
+			enabled: []slog.Level{
+				slog.LevelDebug,
+				slog.LevelError,
+				slog.LevelInfo,
+				slog.LevelWarn,
+			},
+			disabled: []slog.Level{},
+		},
+		// If
+		// 		"pkgslog" level is set to INFO
+		// 		"handler" level is set to WARNING
+		// Then
+		// 		"pkgslog" should accept log levels appropriate
+		// 		to its *minimum* configured log level
+		// Meaning
+		// 		DEBUG level should be rejected
+		//		All other levels should be accepted
+		{
+			name:         "info level",
+			pkgLevel:     slog.LevelInfo,
+			handlerLevel: slog.LevelWarn,
+			enabled: []slog.Level{
+				slog.LevelError,
+				slog.LevelInfo,
+				slog.LevelWarn,
+			},
+			disabled: []slog.Level{
+				slog.LevelDebug,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			var buf bytes.Buffer
+			writer := bufio.NewWriter(&buf)
+
+			textHandler := slog.NewJSONHandler(
+				writer,
+				&slog.HandlerOptions{
+					Level: tt.handlerLevel,
+				},
+			)
+
+			packageMap := map[string]slog.Level{
+				"github.com/reugn/pkgslog": tt.pkgLevel,
+			}
+
+			logger := pkgslog.NewPackageHandler(textHandler, packageMap)
+			for _, checkLevel := range levels {
+				if logger.Enabled(context.TODO(), checkLevel) {
+					if !slices.Contains(tt.enabled, checkLevel) {
+						t.Fatal("expected " + checkLevel.String() + " level to be DISABLED")
+					}
+					continue
+				}
+
+				if !slices.Contains(tt.disabled, checkLevel) {
+					t.Fatal("expected " + checkLevel.String() + " level to be DISABLED")
+				}
+			}
+		})
+	}
+}
+
 func parseLogEntries(t *testing.T, output []byte) []map[string]any {
 	var entries []map[string]any
 	lines := bytes.Split(output, []byte("\n"))
+
 	for i := 0; i < len(lines)-1; i++ { // last one is empty
 		line := lines[i]
 		var entry map[string]any
@@ -63,5 +162,6 @@ func parseLogEntries(t *testing.T, output []byte) []map[string]any {
 		}
 		entries = append(entries, entry)
 	}
+
 	return entries
 }
